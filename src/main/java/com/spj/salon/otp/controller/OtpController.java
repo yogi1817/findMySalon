1ec7fd1e278c18ed48164eedd957f24ae2cf7183
Yogesh Sharma <yogesh4004@example.com>
2020-09-14 22:39:50 -0400

    adding cors

--- src/main/java/com/spj/salon/otp/controller/OtpController.java
+++ src/main/java/com/spj/salon/otp/controller/OtpController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +27,7 @@ import com.spj.salon.user.facade.IUserFacade;
  */
 @Controller
 @RequestMapping(value = "generateOtp", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(origins = "*")
 public class OtpController {
 
 	private static final Logger logger = LogManager.getLogger(OtpController.class.getName());
