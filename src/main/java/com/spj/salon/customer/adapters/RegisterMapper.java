e9e786afe8b90589f5c61d604f3e6e6156f08b60
Yogesh Sharma <yogesh4004@example.com>
2021-02-28 22:54:48 -0500

    updating username to be only lower case

--- src/main/java/com/spj/salon/customer/adapters/RegisterMapper.java
+++ src/main/java/com/spj/salon/customer/adapters/RegisterMapper.java
@@ -15,13 +15,16 @@ import java.util.Locale;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RegisterMapper {
+    @Mapping(source = "email", target = "email", qualifiedByName = "toLowerCase" )
     User toEntity(RegisterCustomerRequest registerCustomerRequest);
 
+    @Mapping(source = "email", target = "email", qualifiedByName = "toLowerCase" )
     RegisterCustomerResponse toResponse(User register);
 
     @Mapping(source = "email", target = "email", qualifiedByName = "toLowerCase" )
     User toEntity(RegisterBarberRequest registerBarberRequest);
 
+    @Mapping(source = "email", target = "email", qualifiedByName = "toLowerCase" )
     RegisterBarberResponse toBarberResponse(User register);
 
     @Named("toLowerCase")
